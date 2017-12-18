4bc3a68a88e569c304b68bde8788d03e8b5e20a9
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-12-18 18:03:27 +0100

    😍

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route(path="/", name="homepage")
+     */
+    public function homepage()
+    {
+        return new Response(
+            $this->get('twig')->render('homepage.html.twig', [
+                'thibaud' => 'He\'s awesome anyway 😎',
+            ])
+        );
+    }
+}
