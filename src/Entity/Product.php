4fb18aa914d737c69b33769c65c2746d8d1f4801
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-12-20 15:36:09 +0100

    Update

---
+++ src/Entity/Product.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ORM\Table(name="product")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=75)
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName(string $name): Product
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
